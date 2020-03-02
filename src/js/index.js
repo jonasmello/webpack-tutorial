9d7ed5a7506c50a22e9cb435a296962750ff5517
Jonas Mello <jonas.mello@dbccompany.com.br>
2020-03-02 11:34:07 -0300

    Added node-sass in webpack.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
+import '../css/styles.scss';
+
 const textComponent = () => {
   const elH11 = document.createElement('h1');
   elH11.innerHTML = 'Olá Mundo!';
+  elH11.classList.add('title');
   return elH11;
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/js/index.js',
@@ -7,10 +8,32 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
     })
   ]
 };
